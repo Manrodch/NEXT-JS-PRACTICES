ed8b25cd464aca2d67882c0ec048899172249170
Manuel Rodríguez <manuel.rodriguez.ch1@gmail.com>
2022-07-21 19:13:30 -0300

    Section A added

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'react';
+import ContentText from '../../components/contentText';
 import TitleMain from '../../components/titleMain';
+import styles from './landing.module.scss';
 
 const Landing: FC = () => {
   return (
     <div>
-      <TitleMain text="2 multimedia Designer breakingtrouhg the fullstack development" />
+      <div className={styles.sectionAstyle}>
+        <div className={styles.cA}>
+          <TitleMain text="2 multimedia Designer breakingtrouhg the fullstack development" />
+          <ContentText text="we are tired of working on this graphics shit in argentina so now we are studying to get out of this fucking place, to fuck whores and smoking pot" />
+        </div>
+        <div className={styles.cB}>imagen</div>
+      </div>
     </div>
   );
 };
